80c39fa42c8c0e1f88f49ac665eb471f0480c136
Rokas <rookas.rudzenskas@gmail.com>
2021-12-11 16:47:34 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>👋 Brainlyemails</title>
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
