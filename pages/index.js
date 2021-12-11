29a93c97616f6ed0158194ae4d6880200f31da58
Rokas <rookas.rudzenskas@gmail.com>
2021-12-11 22:17:01 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import HomeScreen from "../screens/HomeScreen";
 
 export default function Home() {
   return (
-    <div className="bg-black h-screen overflow-y-scroll">
+    <div className="bg-black h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>👋 Brainlyemails</title>
         <link rel="icon" href="../assets/HomeScreen/images/homeScreen_header.png" />
