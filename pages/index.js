dfbd2a5d3db03562a645adfb03c343c3517aaba5
Rokas <rookas.rudzenskas@gmail.com>
2021-12-11 16:53:29 +0200

    Rokas

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
 import Head from 'next/head'
+import HomeScreen from "../screens/HomeScreen";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>👋 Brainlyemails</title>
         <link rel="icon" href="../assets/HomeScreen/images/homeScreen_header.png" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Brainlyemails!
-          </a>
-        </h1>
-
-      </main>
+        <main>
+            <div>
+                <HomeScreen />
+            </div>
+        </main>
 
     </div>
   )
---
+++ screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeScreen = () => {
+    return (
+        <div>
+            Jello
+        </div>
+    );
+};
+
+export default HomeScreen;
