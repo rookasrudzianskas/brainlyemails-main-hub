fa4e3d4301ef0908522fee468a7b59fef5e63d88
Rokas <rookas.rudzenskas@gmail.com>
2021-12-11 21:23:29 +0200

    Rokas

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "../../components/Header";
 import FrontStart from "../../components/FrontStart";
+import Comment from "../../components/Comment";
 
 const HomeScreen = () => {
     return (
