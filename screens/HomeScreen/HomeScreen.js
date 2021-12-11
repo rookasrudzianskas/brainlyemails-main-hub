a62df2dec502800699e454b108cf2209086253d7
Rokas <rookas.rudzenskas@gmail.com>
2021-12-11 16:54:47 +0200

    Rokas

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const HomeScreen = () => {
     return (
-        <div>
-            Jello
+        <div className="">
+            <span className="text-6xl">Hello</span>
         </div>
     );
 };
