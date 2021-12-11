31564c2703eb883093776040c9a54291862ba1e5
Rokas <rookas.rudzenskas@gmail.com>
2021-12-11 21:25:10 +0200

    Rokas

--- components/Comment/Comment.js
+++ components/Comment/Comment.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 const Comment = () => {
     return (
         <div>
-
+            <AcUnitIcon />
         </div>
     );
 };
