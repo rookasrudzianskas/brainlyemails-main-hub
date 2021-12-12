d66d5289bb4aae8e0fd7280ea0104eb6d2432e6d
Rokas <rookas.rudzenskas@gmail.com>
2021-12-12 15:57:20 +0200

    Rokas

--- components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
+++ components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
@@ -4,7 +4,7 @@ const StaticPicture = () => {
     return (
         <div className="flex justify-center overflow-y-hide">
             <div className="relative">
-                <img className="fixed bottom-0 left-40" src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+                <img className="fixed bottom-0 left-[28rem]" src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
             </div>
         </div>
     );
