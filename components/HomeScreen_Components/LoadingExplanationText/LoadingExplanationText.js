1f36ede74696dacf85ed01c43d7afe131e351b3d
Rokas <rookas.rudzenskas@gmail.com>
2021-12-12 14:38:37 +0200

    Rokas

---
+++ components/HomeScreen_Components/LoadingExplanationText/LoadingExplanationText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingExplanationText = () => {
+    return (
+        <div className="">
+
+        </div>
+    );
+};
+
+export default LoadingExplanationText;
