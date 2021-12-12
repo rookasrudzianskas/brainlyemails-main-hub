d4dbc6553d520927aeb3665a25d0ae47af627063
Rokas <rookas.rudzenskas@gmail.com>
2021-12-12 15:54:35 +0200

    Rokas

--- components/HomeScreen_Components/NestedRoutes/ScrollViewWithDetails/ScrollViewWithDetails.js
+++ components/HomeScreen_Components/NestedRoutes/ScrollViewWithDetails/ScrollViewWithDetails.js
@@ -2,8 +2,39 @@ import React from 'react';
 
 const ScrollViewWithDetails = () => {
     return (
-        <div>
-
+        <div className="flex justify-center flex-col items-center max-h-xl overflow-y-scroll">
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
+            <h1>ScrollViewWithDetails</h1>
         </div>
     );
 };
--- components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
+++ components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const StaticPicture = () => {
     return (
-        <div className="">
-
+        <div className="flex justify-center overflow-y-hide">
+            <div className="relative">
+                <img className="fixed bottom-0 left-40" src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
+            </div>
         </div>
     );
 };
