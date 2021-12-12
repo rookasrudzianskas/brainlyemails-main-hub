909f97dcf729133a06d10a6b42ef66dc98e66af3
Rokas <rookas.rudzenskas@gmail.com>
2021-12-12 14:50:03 +0200

    Rokas

--- components/HomeScreen_Components/LoadingExplanationText/LoadingExplanationText.js
+++ components/HomeScreen_Components/LoadingExplanationText/LoadingExplanationText.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingExplanationText = () => {
     return (
-        <div className="p-10 flex items-center justify-center mt-32">
+        <div className="p-10 flex items-center justify-center mt-28">
             <div className=" max-w-4xl p-10 text-7xl font-bold">
                 <span>
                     While you were
