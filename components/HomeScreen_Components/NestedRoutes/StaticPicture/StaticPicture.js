e1e3cc02278153d57f3335ea7b2b0ee050d6a60d
Rokas <rookas.rudzenskas@gmail.com>
2021-12-12 16:06:05 +0200

    Rokas

--- components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
+++ components/HomeScreen_Components/NestedRoutes/StaticPicture/StaticPicture.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const StaticPicture = () => {
+
+    // const [show, handleShow] = useState(false);
+    //
+    //
+    // const transitionNavBar = () => {
+    //     // console.log('This is window 🔥', window.scrollY);
+    //     if(window.scrollY > 400) {
+    //         handleShow(true);
+    //     } else {
+    //         handleShow(false);
+    //     }
+    // }
+    //
+    // useEffect(() => {
+    //     window.addEventListener('scroll', transitionNavBar);
+    //
+    //     return () => window.removeEventListener("scroll", transitionNavBar);
+    // }, []);
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const handleScroll = () => {
+        const position = window.pageYOffset;
+        setScrollPosition(position);
+        console.log(position)
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
         <div className="flex justify-center overflow-y-hide">
-            <div className="relative">
-                <img className="fixed bottom-0 left-[28rem]" src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>
-            </div>
+            {/*<div className="absolute">*/}
+            {/*    <img className="fixed bottom-0 left-[28rem]" src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" alt=""/>*/}
+            {/*</div>*/}
+
+            {/*<div className="App">*/}
+            {/*    {Array(200)*/}
+            {/*        .fill()*/}
+            {/*        .map((_, i) => (*/}
+            {/*            <p key={i}>lorem ipsum {scrollPosition}</p>*/}
+            {/*        ))}*/}
+            {/*</div>*/}
         </div>
     );
 };
